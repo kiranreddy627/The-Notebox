90228183db99c620bce84e73af5c71b0e4f9a8b9
OMKIRAN REDDY <99016756+kiranreddy627@users.noreply.github.com>
2024-07-30 13:19:51 +0530

    Update index.js
    index.js

--- Server/index.js
+++ Server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 app.use(cors(
   {
-      origin: "http://localhost:3001",
+      origin: "http://kiran-the-notebook.vercel.app",
       methods: ['GET', 'POST', 'PUT', 'DELETE'], 
       credentials: true
   }
